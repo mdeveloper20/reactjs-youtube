f47bac040eca6053d9547aab094e4e03a4cebff1
Masoud Pezeshkzade <masoud.pezeshkzade@gmail.com>
2020-03-28 15:51:11 +0430

    add prettier and its configurations

--- src/containers/AppWrapper/AppWrapper.js
+++ src/containers/AppWrapper/AppWrapper.js
@@ -1,65 +1,65 @@
-import React, { Component } from "react";
-import Header from "../../components/Header/Header";
-import MainSidebar from "components/MainSidebar/MainSidebar";
-import SmallSidebar from "../../components/SmallSidebar/SmallSidebar";
+import React, { Component } from 'react'
+import Header from '../../components/Header/Header'
+import MainSidebar from 'components/MainSidebar/MainSidebar'
+import SmallSidebar from '../../components/SmallSidebar/SmallSidebar'
 
 export default class AppWrapper extends Component {
-	state = {
-		wrapperAdditionalClass: "",
-		headerAdditionalClass: ""
-	};
+    state = {
+        wrapperAdditionalClass: '',
+        headerAdditionalClass: '',
+    }
 
-	onMenuClick = () => {
-		const { wrapperAdditionalClass } = this.state;
-		let wrapper = wrapperAdditionalClass;
+    onMenuClick = () => {
+        const { wrapperAdditionalClass } = this.state
+        let wrapper = wrapperAdditionalClass
 
-		if (window.matchMedia("(max-width: 1300px)").matches) {
-			if (wrapperAdditionalClass === "menu_overlay") {
-				wrapper = "";
-			} else {
-				wrapper = "menu_overlay";
-			}
-		} else {
-			if (wrapperAdditionalClass === "menu_small") {
-				wrapper = "";
-			} else {
-				wrapper = "menu_small";
-			}
-		}
-
-		this.setState({
-			wrapperAdditionalClass: wrapper
-		});
-	};
-
-	onSearchBtnClick = () => {
-		console.log("sss");
-        const { headerAdditionalClass } = this.state;
-        let header = headerAdditionalClass;
-
-		if (headerAdditionalClass === "search_active") {
-            header='';
-		} else {
-            header='search_active';
+        if (window.matchMedia('(max-width: 1300px)').matches) {
+            if (wrapperAdditionalClass === 'menu_overlay') {
+                wrapper = ''
+            } else {
+                wrapper = 'menu_overlay'
+            }
+        } else {
+            if (wrapperAdditionalClass === 'menu_small') {
+                wrapper = ''
+            } else {
+                wrapper = 'menu_small'
+            }
         }
-        
-        this.setState({
-            headerAdditionalClass: header
-        })
-	};
 
-	render() {
-		return (
-			<div className={`wrapper ${this.state.wrapperAdditionalClass}`}>
-				<Header
-					onMenuClick={this.onMenuClick}
+        this.setState({
+            wrapperAdditionalClass: wrapper,
+        })
+    }
+
+    onSearchBtnClick = () => {
+        console.log('sss')
+        const { headerAdditionalClass } = this.state
+        let header = headerAdditionalClass
+
+        if (headerAdditionalClass === 'search_active') {
+            header = ''
+        } else {
+            header = 'search_active'
+        }
+
+        this.setState({
+            headerAdditionalClass: header,
+        })
+    }
+
+    render() {
+        return (
+            <div className={`wrapper ${this.state.wrapperAdditionalClass}`}>
+                <Header
+                    onMenuClick={this.onMenuClick}
                     onSearchBtnClick={this.onSearchBtnClick}
                     headerAdditionalClass={this.state.headerAdditionalClass}
-				/>
-				<MainSidebar/>
-				<SmallSidebar/>
-				{this.props.childern}
-			</div>
-		);
-	}
+                />
+                <MainSidebar />
+                <SmallSidebar />
+                {this.props.childern}
+            </div>
+        )
+    }
 }
